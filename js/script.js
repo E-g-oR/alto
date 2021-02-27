350e0e1663899bd1abad014a56af92ce8bf1794d
Egor <68065928+E-g-oR@users.noreply.github.com>
2021-02-27 21:56:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuTop = document.querySelector('.menu-top')
+// const menuTopLink = document.querySelectorAll('.menu-top__link');
 
 menuBtn.addEventListener('click', () => {
    menuBtn.classList.toggle('menu-active');
+
+   menuTop.classList.toggle('menu-active');
 })
